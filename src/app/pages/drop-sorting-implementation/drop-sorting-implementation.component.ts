9178442b82670626dafed03172c4f0732538335a
evheniyrz <reznichenkoevheniy@gmail.com>
2024-04-14 20:49:59 +0300

    cdkdrag broken

--- src/app/pages/drop-sorting-implementation/drop-sorting-implementation.component.ts
+++ src/app/pages/drop-sorting-implementation/drop-sorting-implementation.component.ts
@@ -5,24 +5,29 @@ import { CdkDrag, CdkDropList, CdkDropListGroup, CdkDragDrop } from '@angular/cd
 @Component({
   selector: 'app-drop-sorting-implementation',
   standalone: true,
-  imports: [CdkDrag, CdkDropList, CdkDropListGroup],
+  imports: [ CdkDrag, CdkDropList, CdkDropListGroup],
   templateUrl: './drop-sorting-implementation.component.html',
   styleUrl: './drop-sorting-implementation.component.scss',
   providers:[CdkDrag]
 })
 export class DropSortingImplementationComponent {
   public itemList: DropItem[] = DROP_ITEM_LIST;
+  public targetItems: DropItem[] =[];
+// constructor(private cdkDrag: CdkDrag){}
+  private cdkDrag: CdkDrag = inject(CdkDrag); ///**** HERE*** */
 
-  private cdkDrag: CdkDrag = inject(CdkDrag);
-
-  // @ViewChild('cdkDragRef', { read: CdkDrag })
+  // @ViewChild(CdkDrag, { read: CdkDrag })
   // cdkDragRef!: CdkDrag;
 
   drop(event: CdkDragDrop<DropItem[]>): void {
     if (event.container === event.previousContainer) {
-      console.log('IN SAME', { crr: event.currentIndex, prev: event.previousIndex, dr: this.cdkDrag });
+      console.log('IN SAME', { 
+        crr: event.currentIndex, // with multiline content - index is incorrect
+        prev: event.previousIndex, // with multiline content - index is incorrect
+        dr: this.cdkDrag 
+      });
     
     }
   }
-  dropTarget(event: CdkDragDrop<DropItem>): void {}
+  dropTarget(event: CdkDragDrop<DropItem[]>): void {}
 }
