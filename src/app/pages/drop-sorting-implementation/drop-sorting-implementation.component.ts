2170dde2428829f915e8862f1468ddf5150c3b1a
evgenjnr <evgeniyr7@gmail.com>
2024-05-17 20:27:17 +0300

    rename method in drop-sorting component

--- src/app/pages/drop-sorting-implementation/drop-sorting-implementation.component.ts
+++ src/app/pages/drop-sorting-implementation/drop-sorting-implementation.component.ts
@@ -1,20 +1,24 @@
 import { Component, ViewChild, inject } from '@angular/core';
 import { DROP_ITEM_LIST, DropItem } from './drop-item-list';
-import { CdkDrag, CdkDropList, CdkDropListGroup, CdkDragDrop } from '@angular/cdk/drag-drop';
+import {
+  CdkDrag,
+  CdkDropList,
+  CdkDropListGroup,
+  CdkDragDrop,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-drop-sorting-implementation',
   standalone: true,
-  imports: [ CdkDrag, CdkDropList, CdkDropListGroup],
+  imports: [CdkDrag, CdkDropList, CdkDropListGroup],
   templateUrl: './drop-sorting-implementation.component.html',
   styleUrl: './drop-sorting-implementation.component.scss',
-  providers:[]
+  providers: [],
 })
 export class DropSortingImplementationComponent {
   public itemList: DropItem[] = DROP_ITEM_LIST;
-  public targetItems: DropItem[] =[];
+  public targetItems: DropItem[] = [];
 
-  drop(event: CdkDragDrop<DropItem[]>): void {
-  }
+  dropColumn(event: CdkDragDrop<DropItem[]>): void {}
   dropTarget(event: CdkDragDrop<DropItem[]>): void {}
 }
